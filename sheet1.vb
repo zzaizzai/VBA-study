df8670148d7cd8790fc616f1db59d3e5330f47ae
zzaizzai <junsai0530@gmail.com>
2023-08-27 21:58:22 +0900

    todo

--- sheet1.vb
+++ sheet1.vb
@@ -18,22 +18,40 @@ Sub test()
     Dim textTemp As String
     Dim titles(20) As String
     
+    Dim ws As Worksheet
+    Set ws = Worksheets("Sheet1")
+    ws.Activate
+    Debug.Print ws.Name
+    
+    Dim rowMAx As Long
+    
+    rowMAx = ws.Cells(65536, 2).End(xlUp).row
+    
+    'TODO:
+    'Check Error
+    
+    
+    
+    'TODO: fill col titles with hard coding
     titles(0) = "title"
     titles(1) = "2nd"
     titles(2) = "3nd"
     
+    'save file
     varFile = Application.GetSaveAsFilename(InitialFileName:="test.csv", _
-                                        FileFilter:="CSVｫﾕｫ｡ｫ､ｫ・*.csv),*.csv", _
+                                         FileFilter:="CSV Files (*.csv), *.csv", _
                                         FilterIndex:=1, _
-                                        Title:="ﾜﾁｫﾕｫ｡ｫ､ｫ・ﾎ・")
+                                        Title:="saving")
+    'when you cancled quit sub
     If varFile = False Then
         Exit Sub
     End If
     
+    'saving process
     Open varFile For Output As #1
         Worksheets(1).Select
         
-        'Titles
+        'ouput col titles
         textTemp = titles(0)
         For i = 1 To UBound(titles)
             textTemp = textTemp + "," + titles(i)
@@ -41,42 +59,50 @@ Sub test()
         'textTemp = textTemp + vbCrLf
         Print #1, textTemp
         
-        Dim dataTemp() As Variant
+        
+        'output values
+        Dim dataTemp() As Variant ' temp data row
+        Dim iCol As Long ' col for output
+        Dim iRow As Long ' row for output
+        
+        For iRow = 6 To 10
+            
+            For iCol = 6 To 10
+                
+                dataTemp() = getDataLine(iRow, iCol)
+                
+                ' it there is no value in data tmep, go to continue
+                If IsEmpty(dataTemp(0)) Then
+                    GoTo continue
+                End If
+                
+                Debug.Print "datatemp"
+                textTemp = ""
+                textTemp = dataTemp(0)
+                
+                For i = 1 To UBound(dataTemp)
+                
+                    Debug.Print dataTemp(i)
+                    
+                    If IsEmpty(dataTemp(i)) Then
+                        textTemp = textTemp & "," & ""
+                    Else
+                        textTemp = textTemp & "," & dataTemp(i)
+                    End If
+                Next i
+        
+                Print #1, textTemp
+                
+continue:
+            Next iCol
+        Next iRow
+        
         
         'Values
-        dataTemp() = getDataLine(6, 6)
-        
-        
-        
-        Debug.Print "datatemp"
-        textTemp = ""
-        textTemp = Str(dataTemp(0))
-        For i = 1 To UBound(dataTemp)
-            Debug.Print dataTemp(i)
-            If WorksheetFunction.IsText(dataTemp(1)) Then
-                textTemp = textTemp + "," + dataTemp(i)
-            ElseIf IsNumeric(dataTemp(i)) Then
-                textTemp = textTemp + "," + Str(dataTemp(i))
-            Else
-                textTemp = textTemp + "," + "-1"
-            End If
-        Next
-        
-        
-        Print #1, textTemp
-        
-        
-        
+
         
     Close #1
     
-    
-    
-    
-    'For i = 0 To 20
-     '   aa = getDataLine(6, 6, 5)
-      '  Debug.Print i, aa(i)
-    'Next
 End Sub
 
 Function getDataLine(row As Long, col As Long, Optional rowtitle As Long = 5) As Variant
@@ -96,7 +122,6 @@ Function getDataLine(row As Long, col As Long, Optional rowtitle As Long = 5) As
     Next
     
     
-    
     dataArray(0) = ws.Cells(row, col).Value 'Value
     dataArray(6) = ws.Cells(rowtitle, col).Value 'Date
     
@@ -105,6 +130,9 @@ Function getDataLine(row As Long, col As Long, Optional rowtitle As Long = 5) As
     
 
 End Function
+
+
+
 
 
 
